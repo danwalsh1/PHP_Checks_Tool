dff9fb2ba4d9536663f5a8a845a8c717b327d737
Dan Walsh <dan@d-walsh.co.uk>
2025-01-12 17:11:34 +0000

    Add project files.

---
+++ PHP_Checks_Tool/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPHPStan = New System.Windows.Forms.Button()
+        Me.btnPHPCS = New System.Windows.Forms.Button()
+        Me.btnStatus = New System.Windows.Forms.Button()
+        Me.btnPHPStanCommit = New System.Windows.Forms.Button()
+        Me.btnPHPCSCommit = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSelectDir = New System.Windows.Forms.Button()
+        Me.txtDirectory = New System.Windows.Forms.TextBox()
+        Me.rtbConsole = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'btnPHPStan
+        '
+        Me.btnPHPStan.Location = New System.Drawing.Point(12, 12)
+        Me.btnPHPStan.Name = "btnPHPStan"
+        Me.btnPHPStan.Size = New System.Drawing.Size(75, 23)
+        Me.btnPHPStan.TabIndex = 0
+        Me.btnPHPStan.Text = "PHPStan"
+        Me.btnPHPStan.UseVisualStyleBackColor = True
+        '
+        'btnPHPCS
+        '
+        Me.btnPHPCS.Location = New System.Drawing.Point(93, 12)
+        Me.btnPHPCS.Name = "btnPHPCS"
+        Me.btnPHPCS.Size = New System.Drawing.Size(75, 23)
+        Me.btnPHPCS.TabIndex = 1
+        Me.btnPHPCS.Text = "PHP CS"
+        Me.btnPHPCS.UseVisualStyleBackColor = True
+        '
+        'btnStatus
+        '
+        Me.btnStatus.Location = New System.Drawing.Point(174, 12)
+        Me.btnStatus.Name = "btnStatus"
+        Me.btnStatus.Size = New System.Drawing.Size(75, 23)
+        Me.btnStatus.TabIndex = 2
+        Me.btnStatus.Text = "Status"
+        Me.btnStatus.UseVisualStyleBackColor = True
+        '
+        'btnPHPStanCommit
+        '
+        Me.btnPHPStanCommit.Location = New System.Drawing.Point(255, 12)
+        Me.btnPHPStanCommit.Name = "btnPHPStanCommit"
+        Me.btnPHPStanCommit.Size = New System.Drawing.Size(98, 23)
+        Me.btnPHPStanCommit.TabIndex = 3
+        Me.btnPHPStanCommit.Text = "PHPStan Commit"
+        Me.btnPHPStanCommit.UseVisualStyleBackColor = True
+        '
+        'btnPHPCSCommit
+        '
+        Me.btnPHPCSCommit.Location = New System.Drawing.Point(359, 12)
+        Me.btnPHPCSCommit.Name = "btnPHPCSCommit"
+        Me.btnPHPCSCommit.Size = New System.Drawing.Size(97, 23)
+        Me.btnPHPCSCommit.TabIndex = 4
+        Me.btnPHPCSCommit.Text = "PHP CS Commit"
+        Me.btnPHPCSCommit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Location = New System.Drawing.Point(461, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(2, 23)
+        Me.Label1.TabIndex = 5
+        '
+        'btnSelectDir
+        '
+        Me.btnSelectDir.Location = New System.Drawing.Point(469, 12)
+        Me.btnSelectDir.Name = "btnSelectDir"
+        Me.btnSelectDir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSelectDir.TabIndex = 6
+        Me.btnSelectDir.Text = "Select..."
+        Me.btnSelectDir.UseVisualStyleBackColor = True
+        '
+        'txtDirectory
+        '
+        Me.txtDirectory.Location = New System.Drawing.Point(550, 15)
+        Me.txtDirectory.Name = "txtDirectory"
+        Me.txtDirectory.Size = New System.Drawing.Size(463, 20)
+        Me.txtDirectory.TabIndex = 7
+        '
+        'rtbConsole
+        '
+        Me.rtbConsole.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.rtbConsole.BackColor = System.Drawing.Color.Black
+        Me.rtbConsole.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rtbConsole.ForeColor = System.Drawing.Color.White
+        Me.rtbConsole.Location = New System.Drawing.Point(12, 41)
+        Me.rtbConsole.Name = "rtbConsole"
+        Me.rtbConsole.Size = New System.Drawing.Size(1178, 624)
+        Me.rtbConsole.TabIndex = 8
+        Me.rtbConsole.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1202, 677)
+        Me.Controls.Add(Me.rtbConsole)
+        Me.Controls.Add(Me.txtDirectory)
+        Me.Controls.Add(Me.btnSelectDir)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnPHPCSCommit)
+        Me.Controls.Add(Me.btnPHPStanCommit)
+        Me.Controls.Add(Me.btnStatus)
+        Me.Controls.Add(Me.btnPHPCS)
+        Me.Controls.Add(Me.btnPHPStan)
+        Me.Name = "Form1"
+        Me.Text = "PHP Checks Tool"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnPHPStan As Button
+    Friend WithEvents btnPHPCS As Button
+    Friend WithEvents btnStatus As Button
+    Friend WithEvents btnPHPStanCommit As Button
+    Friend WithEvents btnPHPCSCommit As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnSelectDir As Button
+    Friend WithEvents txtDirectory As TextBox
+    Friend WithEvents rtbConsole As RichTextBox
+End Class
---
+++ PHP_Checks_Tool/Form1.vb
@@ -0,0 +1,173 @@
+﻿Imports System.Diagnostics
+Imports System.IO
+Imports Microsoft.Win32
+Imports System.Text.RegularExpressions
+Imports System.Text
+
+Public Class Form1
+    Private Const RegistryKey As String = "Software\MyPHPTool"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtDirectory.Text = LoadSavedDirectory()
+    End Sub
+
+    Private Function LoadSavedDirectory() As String
+        Using key As RegistryKey = Registry.CurrentUser.OpenSubKey(RegistryKey)
+            If key IsNot Nothing Then
+                Return key.GetValue("DirectoryPath", "").ToString()
+            End If
+        End Using
+        Return ""
+    End Function
+
+    Private Sub SaveDirectory(path As String)
+        Using key As RegistryKey = Registry.CurrentUser.CreateSubKey(RegistryKey)
+            key.SetValue("DirectoryPath", path)
+        End Using
+    End Sub
+
+    Private Sub btnSelectDir_Click(sender As Object, e As EventArgs) Handles btnSelectDir.Click
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            txtDirectory.Text = FolderBrowserDialog1.SelectedPath
+            SaveDirectory(FolderBrowserDialog1.SelectedPath)
+        End If
+    End Sub
+
+    Private Sub RunCommand(command As String)
+        If String.IsNullOrWhiteSpace(txtDirectory.Text) Then
+            MessageBox.Show("Please select a directory first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Try
+            ' Disable buttons before running the command
+            SetButtonsEnabled(False)
+
+            Dim processInfo As New ProcessStartInfo()
+            processInfo.FileName = "cmd.exe"
+            processInfo.Arguments = "/c " & command
+            processInfo.RedirectStandardOutput = True
+            processInfo.RedirectStandardError = True
+            processInfo.UseShellExecute = False
+            processInfo.CreateNoWindow = True
+            processInfo.WorkingDirectory = txtDirectory.Text
+            processInfo.StandardOutputEncoding = System.Text.Encoding.UTF8
+
+            Dim process As New Process()
+            process.StartInfo = processInfo
+
+            ' Clear the console at the start
+            rtbConsole.Invoke(Sub() rtbConsole.Clear())
+
+            ' Store all output lines
+            Dim outputLines As New List(Of String)()
+
+            ' Handle output data as it's received
+            AddHandler process.OutputDataReceived, Sub(sender, e)
+                                                       If e.Data IsNot Nothing Then
+                                                           Dim output As String = StripAnsiCodes(e.Data)
+                                                           My.Application.Log.WriteEntry("OutputDataReceived:")
+                                                           My.Application.Log.WriteEntry(output)
+
+                                                           ' Check if the line contains a progress pattern (e.g., [>])
+                                                           If output.Contains("[>") OrElse output.Contains("[=]") Then
+                                                               ' This is a progress line, update the last line in the console
+                                                               rtbConsole.Invoke(Sub()
+                                                                                     ' Overwrite the last line (if any) with the progress update
+                                                                                     If outputLines.Count > 0 Then
+                                                                                         outputLines(outputLines.Count - 1) = output
+                                                                                     Else
+                                                                                         outputLines.Add(output)
+                                                                                     End If
+                                                                                     ' Clear and re-add all lines (to update the progress)
+                                                                                     rtbConsole.Clear()
+                                                                                     rtbConsole.AppendText(String.Join(Environment.NewLine, outputLines))
+
+                                                                                     ' Ensure the RichTextBox scrolls to the bottom
+                                                                                     rtbConsole.SelectionStart = rtbConsole.Text.Length
+                                                                                     rtbConsole.ScrollToCaret()
+                                                                                 End Sub)
+                                                           Else
+                                                               ' Regular output, append it to the list
+                                                               rtbConsole.Invoke(Sub()
+                                                                                     outputLines.Add(output)
+                                                                                     ' Append the new output normally
+                                                                                     rtbConsole.AppendText(output & Environment.NewLine)
+                                                                                 End Sub)
+                                                           End If
+                                                       End If
+                                                   End Sub
+
+            ' Handle error data
+            AddHandler process.ErrorDataReceived, Sub(sender, e)
+                                                      If e.Data IsNot Nothing Then
+                                                          Dim errorOutput As String = StripAnsiCodes(e.Data)
+                                                          rtbConsole.Invoke(Sub() rtbConsole.AppendText(errorOutput & Environment.NewLine))
+                                                      End If
+                                                  End Sub
+
+            process.Start()
+            process.BeginOutputReadLine()
+            process.BeginErrorReadLine()
+
+            ' Wait for the process to exit and re-enable buttons afterward
+            Task.Run(Sub()
+                         process.WaitForExit()
+                         process.Dispose()
+
+                         ' Re-enable buttons after the process finishes
+                         rtbConsole.Invoke(Sub() SetButtonsEnabled(True))
+                     End Sub)
+
+        Catch ex As Exception
+            MessageBox.Show("Error running command: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+
+
+
+
+
+
+
+
+    Private Sub OutputHandler(sender As Object, e As DataReceivedEventArgs)
+        If e.Data IsNot Nothing Then
+            rtbConsole.Invoke(Sub() rtbConsole.AppendText(e.Data & Environment.NewLine))
+        End If
+    End Sub
+
+    Private Sub btnPHPStan_Click(sender As Object, e As EventArgs) Handles btnPHPStan.Click
+        RunCommand(".\vendor\bin\phpstan analyse --configuration=phpstan.neon --memory-limit=1G -v")
+    End Sub
+
+    Private Sub btnPHPCS_Click(sender As Object, e As EventArgs) Handles btnPHPCS.Click
+        RunCommand(".\vendor\bin\phpcs --standard=phpcs.xml src")
+    End Sub
+
+    Private Sub btnStatus_Click(sender As Object, e As EventArgs) Handles btnStatus.Click
+        RunCommand("git status")
+    End Sub
+
+    Private Sub btnPHPStanCommit_Click(sender As Object, e As EventArgs) Handles btnPHPStanCommit.Click
+        RunCommand("git add . && git commit -m ""Altered code to ensure PHPStan level 8 compliance""")
+    End Sub
+
+    Private Sub btnPHPCSCommit_Click(sender As Object, e As EventArgs) Handles btnPHPCSCommit.Click
+        RunCommand("git add . && git commit -m ""Altered code to ensure PSR-12 compliance""")
+    End Sub
+
+    Private Function StripAnsiCodes(input As String) As String
+        ' Regex pattern to match ANSI escape codes
+        Dim ansiEscape As New Regex("\x1B\[[0-9;]*[A-Za-z]")
+        Return ansiEscape.Replace(input, "")
+    End Function
+
+    Private Sub SetButtonsEnabled(enabled As Boolean)
+        btnPHPStan.Invoke(Sub() btnPHPStan.Enabled = enabled)
+        btnPHPCS.Invoke(Sub() btnPHPCS.Enabled = enabled)
+        btnStatus.Invoke(Sub() btnStatus.Enabled = enabled)
+        btnPHPStanCommit.Invoke(Sub() btnPHPStanCommit.Enabled = enabled)
+        btnPHPCSCommit.Invoke(Sub() btnPHPCSCommit.Enabled = enabled)
+    End Sub
+End Class
